c8864c30bcc24ad9f2df5605a31bc33b55a6f8b7
Rails Udemy Course <jonathanli@wiredhere.com>
2016-05-22 06:17:33 +0000

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
+
 
   def index
   @users = User.paginate(page: params[:page], per_page: 5)
@@ -25,14 +28,11 @@ class UsersController < ApplicationController
 
   def edit
 
-  @user = User.find(params[:id])
-
   end
 
   def update
-  @user = User.find(params[:id])
 
-  if @user.update(user_params)
+    if @user.update(user_params)
      flash[:sucess] = "Your account was updated sucessfully"
     redirect_to articles_path
   else
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
  end
 
     def show
-    @user = User.find(params[:id])
+
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
 
@@ -53,5 +53,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
 
   end
+  def set_user
+    @user = User.find(params[:id])
+end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+
+  end
+
+
 
 end
