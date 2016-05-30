0eaa281112e8660365c6ca19f75c7e3ce25db2e3
Rails Udemy Course <jonathanli@wiredhere.com>
2016-05-30 01:59:43 +0000

    Add edit categories feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,10 +25,25 @@ def create
 else
 
     render 'new'
+  end
+end
+
+def edit
+  @category = Category.find(params[:id])
+
 
 end
 
-end
+  def update
+  @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:sucess] = "Category name was sucessfully updated"
+      redirect_to category_path(@category)
+      else
+      render 'edit'
+    end
+
+  end
 
 def show
   @category = Category.find(params[:id])
